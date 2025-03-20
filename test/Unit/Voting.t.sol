65a98ab335954393a6f4945415a114a3a1a21662
rbd3 <rabedesanaandry01@gmail.com>
2025-03-20 22:02:06 +0300

    Fix error

--- test/Unit/Voting.t.sol
+++ test/Unit/Voting.t.sol
@@ -3,5 +3,9 @@ pragma solidity ^0.8.28;
 
 import {Test} from "forge-std/Test.sol";
 import {Vm} from "forge-std/Vm.sol";
-import {DeployBallot} from "script/DeployRaffle.s.sol";
+//import {DeployBallot} from "script/DeployRaffle.s.sol";
 import {Ballot} from "src/Voting.sol";
+
+contract BallotTest is Test {
+    Ballot public ballot;
+}
