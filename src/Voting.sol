8b53a8f8708bd743322216e60e3395d3800c5eb1
rbd3 <rabedesanaandry01@gmail.com>
2025-03-20 19:37:39 +0300

    Add a constructor for contract

--- src/Voting.sol
+++ src/Voting.sol
@@ -1,2 +1,42 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
+
+contract Ballot {
+    /*
+    struct voter, proposal
+    function give right to vote, delegate vote, winning proposal, winner name,
+    event voted
+    */
+
+    struct Voter{
+        uint256 weight,
+        uint vote,
+        address delegate,
+        bool voted
+    }
+    struct Proposal{
+        bytes32 name,
+        uint voteCount
+    }
+
+    address public chairperson;
+    mapping(uint256=>Voter) public voters;
+    Proposals[] public proposals;
+
+    /* Error */
+    error Ballot__NoProposaleProvided();
+
+    constructor(bytes32 memory proposaleName){
+        require(proposaleName.length > 0, Ballot__NoProposaleProvided());
+        chairperson = msg.sender;
+        voters[chairperson].weight = 1;
+        for(uint i = 0; i < proposalNames.length; i++) {
+            for (uint j = i + 1; j < proposalNames.length; j++) { //Ensure proposal names are unique to prevent ambiguity.
+            require(proposalNames[i] != proposalNames[j], "Duplicate proposal names");
+        }
+        proposals.push(Proposal({
+            name: proposalNames[i],
+            voteCount: 1
+        }));
+    }
+}
