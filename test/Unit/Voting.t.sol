fae0dfd046aa2ca548e17c9894dc33e9688d62cd
rbd3 <rabedesanaandry01@gmail.com>
2025-03-20 21:56:16 +0300

    Update code

---
+++ test/Unit/Voting.t.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import {Test} from "forge-std/Test.sol";
+import {Vm} from "forge-std/Vm.sol";
+import {DeployBallot} from "script/DeployRaffle.s.sol";
+import {Ballot} from "src/Voting.sol";
